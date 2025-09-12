0fa86d49df42d8333e1ca9b42aef0825ff091d4b
박진우 <stryperhan50@gmail.com>
2025-09-12 15:16:59 +0900

    feat: Create Header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* 다른 컴포넌트들이 여기에 추가될 수 있습니다. */}
     </div>
   );
 }
